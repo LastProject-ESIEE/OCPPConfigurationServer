12bb40f2ad21a3b32577213b6365d6bcfce52cef
Thomas VELU <velu.thomas77@laposte.net>
2024-02-13 15:49:03 +0100

    fix : checkstyle

--- src/main/java/fr/uge/chargepointconfiguration/chargepoint/WebSocketRequestMessage.java
+++ src/main/java/fr/uge/chargepointconfiguration/chargepoint/WebSocketRequestMessage.java
@@ -1,22 +1,34 @@
 package fr.uge.chargepointconfiguration.chargepoint;
 
-public record WebSocketRequestMessage(int callType, String messageId, String messageName, String data) {
+/**
+ * A record to define the RequestMessage packet.
+ *
+ * @param callType    An int.
+ * @param messageId   A String.
+ * @param messageName A String.
+ * @param data        A String.
+ */
+public record WebSocketRequestMessage(int callType,
+                                      String messageId,
+                                      String messageName,
+                                      String data) {
 
-    /**
-     * Parse string message received from the web socket
-     * @param message - received message
-     * @return WebSocketMessage
-     */
-    public static WebSocketRequestMessage parse(String message){
-        var array = message.substring(1,message.length() - 1).split(",",4);
-        int callType = Integer.parseInt(array[0]);
-        String messageId = array[1];
-        String messageName = array[2].substring(1, array[2].length() - 1);
-        return new WebSocketRequestMessage(callType, messageId, messageName, array[3]);
-    }
+  /**
+   * Parse string message received from the web socket.
+   *
+   * @param message - received message
+   * @return WebSocketMessage
+   */
+  public static WebSocketRequestMessage parse(String message) {
+    var array = message.substring(1, message.length() - 1).split(",", 4);
+    int callType = Integer.parseInt(array[0]);
+    String messageId = array[1];
+    String messageName = array[2].substring(1, array[2].length() - 1);
+    return new WebSocketRequestMessage(callType, messageId, messageName, array[3]);
+  }
 
-    @Override
-    public String toString(){
-        return "[" + callType + ",\"" + messageId + "\",\"" + messageName + "\"," + data+ "]";
-    }
+  @Override
+  public String toString() {
+    return "[" + callType + ",\"" + messageId + "\",\"" + messageName + "\"," + data + "]";
+  }
 }
