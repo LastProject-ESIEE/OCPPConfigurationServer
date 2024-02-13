4aff3d09f6d0a92fa2f104ec30caffcea3e6f1cf
Thomas VELU <velu.thomas77@laposte.net>
2024-02-13 15:57:47 +0100

    fix : checkstyle

---
+++ src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp16/OcppConfigurationMessageVisitor16.java
@@ -0,0 +1,12 @@
+package fr.uge.chargepointconfiguration.chargepoint.ocpp.ocpp16;
+
+/**
+ * Defines the OCPP configuration message for the visitor.
+ */
+public class OcppConfigurationMessageVisitor16 implements OCPPMessageVisitor16 {
+
+  @Override
+  public void visit(BootNotificationRequest bootNotificationRequest) {
+  }
+
+}
