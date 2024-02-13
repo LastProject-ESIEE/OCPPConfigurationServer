d287df75c38c2df29719bfacef81227f5b878397
Thomas VELU <velu.thomas77@laposte.net>
2024-02-13 15:23:53 +0100

    fix : checkstyle

--- src/main/java/fr/uge/chargepointconfiguration/MyHandler.java
+++ src/main/java/fr/uge/chargepointconfiguration/MyHandler.java
@@ -4,11 +4,13 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+/**
+ * Defines a custom Handler to handle messages received from the web session.
+ */
 public class MyHandler extends TextWebSocketHandler {
 
-	@Override
-	public void handleTextMessage(WebSocketSession session, TextMessage message) {
+  @Override
+  public void handleTextMessage(WebSocketSession session, TextMessage message) {
 
-	}
-
+  }
 }
