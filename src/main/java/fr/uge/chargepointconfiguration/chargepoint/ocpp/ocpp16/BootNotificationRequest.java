dca1f9e40ade0cc23bba68370a8c50add276f903
Thomas VELU <velu.thomas77@laposte.net>
2024-02-13 16:05:55 +0100

    fix : checkstyle

--- src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp16/BootNotificationRequest.java
+++ src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp16/BootNotificationRequest.java
@@ -2,6 +2,19 @@ package fr.uge.chargepointconfiguration.chargepoint.ocpp.ocpp16;
 
 import fr.uge.chargepointconfiguration.chargepoint.ocpp.OcppMessage;
 
-public record BootNotificationRequest(String chargePointVendor, String chargePointModel, String chargePointSerialNumber, String chargeBoxSerialNumber, String firmwareVersion) implements OcppMessage {
+/**
+ * Defines the BootNotificationRequest message from the OCPP protocol.
+ *
+ * @param chargePointVendor String.
+ * @param chargePointModel String.
+ * @param chargePointSerialNumber String.
+ * @param chargeBoxSerialNumber String.
+ * @param firmwareVersion String.
+ */
+public record BootNotificationRequest(String chargePointVendor,
+                                      String chargePointModel,
+                                      String chargePointSerialNumber,
+                                      String chargeBoxSerialNumber,
+                                      String firmwareVersion) implements OcppMessage {
 
 }
