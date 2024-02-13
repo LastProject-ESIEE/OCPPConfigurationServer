57e2b29f24c7e49270162fecfca134ca619d8b4e
Thomas VELU <velu.thomas77@laposte.net>
2024-02-13 15:35:12 +0100

    fix : checkstyle

--- src/main/java/fr/uge/chargepointconfiguration/controller/FirmwareController.java
+++ src/main/java/fr/uge/chargepointconfiguration/controller/FirmwareController.java
@@ -2,33 +2,51 @@ package fr.uge.chargepointconfiguration.controller;
 
 import fr.uge.chargepointconfiguration.entities.Firmware;
 import fr.uge.chargepointconfiguration.repository.FirmwareRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
+/**
+ * A controller for the firmware entity.
+ */
 @RestController
 public class FirmwareController {
+
   private final FirmwareRepository firmwareRepository;
 
+  /**
+   * FirmwareController's constructor.
+   *
+   * @param firmwareRepository A FirmwareRepository.
+   */
   @Autowired
   public FirmwareController(FirmwareRepository firmwareRepository) {
     this.firmwareRepository = firmwareRepository;
   }
 
+  /**
+   * Returns all the firmwares.
+   *
+   * @return A list of all the firmwares.
+   */
   @GetMapping(value = "/firmwares")
-  public List<Firmware> getAllChargepoints(){
-//    ArrayList<Firmware> firmwares = new ArrayList<>();
-//    firmwareRepository.findAll().forEach(firmwares::add);
-//    return firmwares;
-    return StreamSupport.stream(firmwareRepository.findAll().spliterator(), false).collect(Collectors.toList());
+  public List<Firmware> getAllChargepoints() {
+    return StreamSupport.stream(firmwareRepository.findAll().spliterator(), false)
+            .collect(Collectors.toList());
   }
 
+  /**
+   * Returns a firmware according to the given id.<br>
+   * The optional will be empty if a firmware could not be found.
+   *
+   * @param id An int.
+   * @return An optional of firmware.
+   */
   @GetMapping(value = "/firmwares/{id}")
   public Optional<Firmware> getChargepointById(@PathVariable int id) {
     return firmwareRepository.findById(id);
