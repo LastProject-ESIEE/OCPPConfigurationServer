6986c8270e27112eed1004163365ced0e83a9d92
Thomas VELU <velu.thomas77@laposte.net>
2024-02-13 15:38:51 +0100

    fix : checkstyle

--- src/main/java/fr/uge/chargepointconfiguration/chargepoint/MessageSender.java
+++ src/main/java/fr/uge/chargepointconfiguration/chargepoint/MessageSender.java
@@ -1,8 +1,9 @@
 package fr.uge.chargepointconfiguration.chargepoint;
 
-import fr.uge.chargepointconfiguration.chargepoint.ocpp.OCPPMessage;
-
+/**
+ * An interface to defines a message sender via a web socket.
+ */
 @FunctionalInterface
 public interface MessageSender {
-    void sendMessage(WebSocketResponseMessage webSocketResponseMessage);
+  void sendMessage(WebSocketResponseMessage webSocketResponseMessage);
 }
