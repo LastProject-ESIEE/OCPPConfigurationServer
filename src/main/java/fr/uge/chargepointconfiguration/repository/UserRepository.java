68ff56224ea9fe4d3f04f516b80a13d69589bb1b
Thomas VELU <velu.thomas77@laposte.net>
2024-02-13 16:18:10 +0100

    fix : checkstyle

--- src/main/java/fr/uge/chargepointconfiguration/repository/UserRepository.java
+++ src/main/java/fr/uge/chargepointconfiguration/repository/UserRepository.java
@@ -4,5 +4,8 @@ import fr.uge.chargepointconfiguration.entities.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository for the user.
+ */
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {}
