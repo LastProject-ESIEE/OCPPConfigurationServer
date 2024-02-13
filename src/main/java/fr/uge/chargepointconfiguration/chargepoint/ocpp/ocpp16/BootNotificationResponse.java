1b24f4e314c39d68a00e620474bda6723e2ea7f7
Thomas VELU <velu.thomas77@laposte.net>
2024-02-13 16:02:45 +0100

    fix : checkstyle

--- src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp16/BootNotificationResponse.java
+++ src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp16/BootNotificationResponse.java
@@ -2,6 +2,14 @@ package fr.uge.chargepointconfiguration.chargepoint.ocpp.ocpp16;
 
 import fr.uge.chargepointconfiguration.chargepoint.ocpp.OcppMessage;
 
-
-public record BootNotificationResponse(String currentTime, int interval, RegistrationStatus status) implements OcppMessage {
+/**
+ * Defines a response to a BootNotification message.
+ *
+ * @param currentTime String.
+ * @param interval An int.
+ * @param status RegistrationStatus.
+ */
+public record BootNotificationResponse(String currentTime,
+                                       int interval,
+                                       RegistrationStatus status) implements OcppMessage {
 }
