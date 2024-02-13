bfebd1c2b50638e5825219b72a570ff7df4fd407
Thomas VELU <velu.thomas77@laposte.net>
2024-02-13 16:00:25 +0100

    fix : checkstyle

--- src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp16/ChangeConfiguration.java
+++ src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp16/ChangeConfiguration.java
@@ -2,5 +2,11 @@ package fr.uge.chargepointconfiguration.chargepoint.ocpp.ocpp16;
 
 import fr.uge.chargepointconfiguration.chargepoint.ocpp.OcppMessage;
 
+/**
+ * An OCCP message to change a field value.
+ *
+ * @param key String.
+ * @param value String.
+ */
 public record ChangeConfiguration(String key, String value) implements OcppMessage {
 }
