dd83d86dd362770b46390456292b0eaec0fbb0e8
Thomas VELU <velu.thomas77@laposte.net>
2024-02-13 15:26:28 +0100

    fix : checkstyle

--- src/main/java/fr/uge/chargepointconfiguration/ChargepointconfigurationApplication.java
+++ src/main/java/fr/uge/chargepointconfiguration/ChargepointconfigurationApplication.java
@@ -2,12 +2,12 @@ package fr.uge.chargepointconfiguration;
 
 import fr.uge.chargepointconfiguration.chargepoint.ConfigurationServer;
 import fr.uge.chargepointconfiguration.repository.UserRepository;
+import java.net.InetSocketAddress;
 import org.java_websocket.server.WebSocketServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.net.InetSocketAddress;
 
 /**
  * The main entry point to the application.<br>
@@ -16,36 +16,36 @@ import java.net.InetSocketAddress;
 @SpringBootApplication
 public class ChargepointconfigurationApplication implements CommandLineRunner {
 
-	private final UserRepository userRepository;
+  private final UserRepository userRepository;
 
-	/**
-	 * The class's constructor.<br>
-	 * It requires the UserRepository, but it is auto-wired thanks to Spring Boot.
-	 *
-	 * @param userRepository UserRepository.
-	 */
-	@Autowired
-	public ChargepointconfigurationApplication(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
+  /**
+   * The class's constructor.<br>
+   * It requires the UserRepository, but it is auto-wired thanks to Spring Boot.
+   *
+   * @param userRepository UserRepository.
+   */
+  @Autowired
+  public ChargepointconfigurationApplication(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
 
-	/**
-	 * Launches the server by instantiating the application and running it.
-	 *
-	 * @param args String[].
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(ChargepointconfigurationApplication.class, args);
-	}
+  /**
+   * Launches the server by instantiating the application and running it.
+   *
+   * @param args String[].
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(ChargepointconfigurationApplication.class, args);
+  }
 
-	@Override
-	public void run(String... args) throws Exception {
-		Thread.ofPlatform().start(() -> {
-			var host = "192.168.0.5";
-			var port = 8887;
-			WebSocketServer server = new ConfigurationServer(new InetSocketAddress(host, port),
-							userRepository);
-			server.run();
-		});
-	}
+  @Override
+  public void run(String... args) throws Exception {
+    Thread.ofPlatform().start(() -> {
+      var host = "192.168.0.5";
+      var port = 8887;
+      WebSocketServer server = new ConfigurationServer(new InetSocketAddress(host, port),
+              userRepository);
+      server.run();
+    });
+  }
 }
