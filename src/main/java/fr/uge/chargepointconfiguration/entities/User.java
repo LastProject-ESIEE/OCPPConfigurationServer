68f2b0b72ca13c10ba53a01ba2d1d5c0bd68a870
Thomas VELU <velu.thomas77@laposte.net>
2024-02-13 10:36:32 +0100

    fix : codacy's checkstyle error

--- src/main/java/fr/uge/chargepointconfiguration/entities/User.java
+++ src/main/java/fr/uge/chargepointconfiguration/entities/User.java
@@ -2,8 +2,8 @@ package fr.uge.chargepointconfiguration.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
