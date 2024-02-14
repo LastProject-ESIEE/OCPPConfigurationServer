8b42419c0ab9f43ab5944f86332ee6af3a10816d
Thomas VELU <velu.thomas77@laposte.net>
2024-02-14 15:47:26 +0100

    add : class to define what to parse from json to java object when receiving the BootNotificationRequest

---
+++ src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp2/BootNotificationRequest.java
@@ -0,0 +1,15 @@
+package fr.uge.chargepointconfiguration.chargepoint.ocpp.ocpp2;
+
+import fr.uge.chargepointconfiguration.chargepoint.ocpp.OcppMessage;
+import fr.uge.chargepointconfiguration.chargepoint.ocpp.ocpp2.data.ChargingStation;
+
+/**
+ * Defines the BootNotificationRequest message from the OCPP protocol.
+ *
+ * @param chargingStation ChargingStation information.
+ * @param reason String, the reason of why this message has been sent.
+ */
+public record BootNotificationRequest(ChargingStation chargingStation,
+                                      String reason) implements OcppMessage {
+
+}
