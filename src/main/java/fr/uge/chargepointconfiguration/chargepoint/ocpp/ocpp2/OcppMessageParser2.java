cb114a47dc6218338b0f7cacc61719c998610c61
Thomas VELU <velu.thomas77@laposte.net>
2024-02-14 15:12:03 +0100

    add : message parser for the OCPP 2.0.1, we need to modify it to really accept BootNotification

---
+++ src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp2/OcppMessageParser2.java
@@ -0,0 +1,29 @@
+package fr.uge.chargepointconfiguration.chargepoint.ocpp.ocpp2;
+
+import fr.uge.chargepointconfiguration.chargepoint.WebSocketRequestMessage;
+import fr.uge.chargepointconfiguration.chargepoint.ocpp.OcppMessage;
+import fr.uge.chargepointconfiguration.chargepoint.ocpp.OcppMessageParser;
+import fr.uge.chargepointconfiguration.chargepoint.ocpp.ocpp16.BootNotificationRequest;
+import fr.uge.chargepointconfiguration.tools.JsonParser;
+
+/**
+ * Parses an OCPP 2.0.1 message.
+ */
+public class OcppMessageParser2 implements OcppMessageParser {
+
+  @Override
+  public OcppMessage parseMessage(WebSocketRequestMessage webSocketRequestMessage) {
+    return switch (webSocketRequestMessage.messageName()) {
+      case "BootNotification" ->
+              JsonParser.stringToObject(BootNotificationRequest.class,
+                      webSocketRequestMessage.data());
+      default -> throw new IllegalArgumentException("Message not recognized: "
+              + webSocketRequestMessage);
+    };
+  }
+
+  @Override
+  public String transform(OcppMessage message) {
+    return JsonParser.objectToJsonString(message);
+  }
+}
