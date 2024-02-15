7755f63d80d4ebf10453e8f55a60fb614f762140
Thomas VELU <velu.thomas77@laposte.net>
2024-02-15 12:03:37 +0100

    add : user details service for the user's authentication on the app

---
+++ src/main/java/fr/uge/chargepointconfiguration/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package fr.uge.chargepointconfiguration.security;
+
+import fr.uge.chargepointconfiguration.repository.UserRepository;
+import java.util.Objects;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * Loads the information of the connected user.
+ */
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  public UserDetailsServiceImpl(UserRepository userRepository) {
+    Objects.requireNonNull(userRepository);
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) {
+    var user = userRepository.findByEmail(username);
+    if (user == null) {
+      throw new UsernameNotFoundException("User does not exist");
+    }
+    return new UserDetailsImpl(user);
+  }
+}
