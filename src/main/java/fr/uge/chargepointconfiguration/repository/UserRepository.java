40c0694731a84eed0fb80a909190e56b580a7af3
Thomas VELU <velu.thomas77@laposte.net>
2024-02-15 12:04:41 +0100

    add : a query to retrieve a user with his email

--- src/main/java/fr/uge/chargepointconfiguration/repository/UserRepository.java
+++ src/main/java/fr/uge/chargepointconfiguration/repository/UserRepository.java
@@ -1,11 +1,24 @@
 package fr.uge.chargepointconfiguration.repository;
 
 import fr.uge.chargepointconfiguration.entities.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
  * Repository for the user.
  */
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {}
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+  /**
+   * Returns a User from the database according to the email.
+   *
+   * @param email User's email.
+   * @return The correct User or null if the user couldn't be found.
+   */
+  @Query("SELECT u FROM User u WHERE u.email = :email")
+  User findByEmail(@Param("email") String email);
+
+}
