e64e9a594432208415b91d0cd8e62614366048a4
Thomas VELU <velu.thomas77@laposte.net>
2024-02-15 12:04:52 +0100

    add : authentication to the app

--- src/main/java/fr/uge/chargepointconfiguration/security/WebSecurityConfig.java
+++ src/main/java/fr/uge/chargepointconfiguration/security/WebSecurityConfig.java
@@ -1,16 +1,17 @@
 package fr.uge.chargepointconfiguration.security;
 
+import fr.uge.chargepointconfiguration.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -60,20 +61,27 @@ public class WebSecurityConfig {
     return http.build();
   }
 
+  @Autowired
   @Bean
-  InMemoryUserDetailsManager userDetailsService() {
-    UserDetails admin = User.withUsername("admin")
-            .passwordEncoder(passwordEncoder()::encode)
-            .password("password")
-            .roles("ADMIN")
-            .build();
-    UserDetails user = User.withUsername("user")
-            .passwordEncoder(passwordEncoder()::encode)
-            .password("password")
-            .roles("USER")
-            .build();
-    return new InMemoryUserDetailsManager(admin, user);
+  UserDetailsService userDetailsService(UserRepository userRepository) {
+    return new UserDetailsServiceImpl(userRepository);
   }
+
+  /**
+   * SHOULD BE REFACTORED, CALLING TWO AUTOWIRED FOR A REPOSITORY :(.
+   *
+   * @param userRepository The user's repository.
+   * @return An authentication provider for the user.
+   */
+  @Autowired
+  @Bean
+  public DaoAuthenticationProvider authenticationProvider(UserRepository userRepository) {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService(userRepository));
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
+
 
   @Bean
   public PasswordEncoder passwordEncoder() {
