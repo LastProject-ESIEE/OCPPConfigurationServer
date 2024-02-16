a5356b619c9c3cef0fe02b344ccaa9fdf89ba0bc
Thomas VELU <velu.thomas77@laposte.net>
2024-02-16 10:37:58 +0100

    add : javadoc for the BootNotificationResponse v2.0

--- src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp2/BootNotificationResponse.java
+++ src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp2/BootNotificationResponse.java
@@ -4,11 +4,16 @@ import fr.uge.chargepointconfiguration.chargepoint.ocpp.OcppMessage;
 import fr.uge.chargepointconfiguration.chargepoint.ocpp.RegistrationStatus;
 
 /**
- * Defines a response to a BootNotification message.
+ * Defines a response to the BootNotificationRequest.<br>
+ * According to the OCPP documentation, the server should send a BootNotificationResponse
+ * to confirm that we had read the message.<br>
+ * See this as an ACK packet.
  *
- * @param currentTime String.
- * @param interval An int.
- * @param status RegistrationStatusEnumType.
+ * @param currentTime The current server time of the server in String.
+ * @param interval This value defines the interval in second that the remote should respect
+ *                 before sending a HeartBeat packet (which is like a PING).
+ * @param status This enum serves as a response for the message. If it is accepted,
+ *               rejected or something else.
  */
 public record BootNotificationResponse(String currentTime,
                                        int interval,
