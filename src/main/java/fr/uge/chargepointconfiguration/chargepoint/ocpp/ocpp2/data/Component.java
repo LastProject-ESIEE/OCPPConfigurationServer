e2329e18af4659634cbc2a1fb07429fd9d729655
Thomas VELU <velu.thomas77@laposte.net>
2024-02-16 10:41:38 +0100

    delete : useless classes

--- src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp2/data/Component.java
+++
@@ -1,9 +0,0 @@
-package fr.uge.chargepointconfiguration.chargepoint.ocpp.ocpp2.data;
-
-/**
- * Defines a component of the machine.
- *
- * @param name The name of the machine's component.
- */
-public record Component(String name) {
-}
--- src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp2/data/VariableType.java
+++
@@ -1,9 +0,0 @@
-package fr.uge.chargepointconfiguration.chargepoint.ocpp.ocpp2.data;
-
-/**
- * Defines the variable of the machine's component.
- *
- * @param name Name of the variable.
- */
-public record VariableType(String name) {
-}
