f3c0e3235868b4146fb884d2a53b0d66275d59d6
Thomas VELU <velu.thomas77@laposte.net>
2024-02-17 14:38:17 +0100

    add : a method to find a chargepoint thanks to his serial number

--- src/main/java/fr/uge/chargepointconfiguration/repository/ChargepointRepository.java
+++ src/main/java/fr/uge/chargepointconfiguration/repository/ChargepointRepository.java
@@ -1,7 +1,9 @@
 package fr.uge.chargepointconfiguration.repository;
 
 import fr.uge.chargepointconfiguration.entities.Chargepoint;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,4 +11,13 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface ChargepointRepository extends CrudRepository<Chargepoint, Integer> {
+
+  /**
+   * Returns a Chargepoint from the database according to the serial number.
+   *
+   * @param serialNumber Chargepoint's unique serial number.
+   * @return The correct Chargepoint or null if the chargepoint couldn't be found.
+   */
+  @Query("SELECT c FROM Chargepoint c WHERE c.serialNumberChargepoint = :serialNumber")
+  Chargepoint findBySerialNumber(@Param("serialNumber") String serialNumber);
 }
