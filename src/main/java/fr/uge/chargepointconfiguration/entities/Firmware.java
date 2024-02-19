29f332b1f29499a93bbd89f20935d7d10d71183f
Thomas VELU <velu.thomas77@laposte.net>
2024-02-19 17:20:00 +0100

    fix : many to many relation

--- src/main/java/fr/uge/chargepointconfiguration/entities/Firmware.java
+++ src/main/java/fr/uge/chargepointconfiguration/entities/Firmware.java
@@ -1,7 +1,9 @@
 package fr.uge.chargepointconfiguration.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -35,10 +37,10 @@ public class Firmware {
   @Column(name = "constructor", nullable = false, length = 45)
   private String constructor;
 
-  @ManyToMany
+  @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
   @JoinTable(name = "compatibility",
-          joinColumns = @JoinColumn(name = "id_firmware"),
-          inverseJoinColumns = @JoinColumn(name = "id_type_allowed"))
+          joinColumns = @JoinColumn(name = "id_type_allowed"),
+          inverseJoinColumns = @JoinColumn(name = "id_firmware"))
   private Set<TypeAllowed> typesAllowed;
 
   public int getId() {
