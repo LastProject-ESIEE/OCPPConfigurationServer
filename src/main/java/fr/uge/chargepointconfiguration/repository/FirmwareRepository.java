9dd9405cf5dac0c37e9e122516141f34ba288e4c
Thomas VELU <velu.thomas77@laposte.net>
2024-02-19 17:19:23 +0100

    add : query to fetch a firmware according to his version

--- src/main/java/fr/uge/chargepointconfiguration/repository/FirmwareRepository.java
+++ src/main/java/fr/uge/chargepointconfiguration/repository/FirmwareRepository.java
@@ -1,10 +1,21 @@
 package fr.uge.chargepointconfiguration.repository;
 
 import fr.uge.chargepointconfiguration.entities.Firmware;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Repository for the firmware.
  */
 public interface FirmwareRepository extends CrudRepository<Firmware, Integer> {
+
+  /**
+   * Returns a Firmware from the database according to the version.
+   *
+   * @param version Firmware's unique version.
+   * @return The correct Firmware or null if the firmware couldn't be found.
+   */
+  @Query("SELECT f FROM Firmware f WHERE f.version LIKE :version")
+  Firmware findByVersion(@Param("version") String version);
 }
