fc8bdcfc0fe0318cf5a6b6d17c85e7d40caf149d
Marius <marius.donne@edu.univ-eiffel.fr>
2024-02-20 11:59:15 +0100

    Add 2.0 configuration observer

--- src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/OcppObserver.java
+++ src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/OcppObserver.java
@@ -3,6 +3,7 @@ package fr.uge.chargepointconfiguration.chargepoint.ocpp;
 import fr.uge.chargepointconfiguration.chargepoint.ChargePointManager;
 import fr.uge.chargepointconfiguration.chargepoint.OcppMessageSender;
 import fr.uge.chargepointconfiguration.chargepoint.ocpp.ocpp16.Ocpp16ConfigurationObserver;
+import fr.uge.chargepointconfiguration.chargepoint.ocpp.ocpp2.OcppConfigurationObserver2;
 import fr.uge.chargepointconfiguration.repository.ChargepointRepository;
 import fr.uge.chargepointconfiguration.repository.FirmwareRepository;
 import fr.uge.chargepointconfiguration.repository.StatusRepository;
@@ -11,14 +12,15 @@ import fr.uge.chargepointconfiguration.repository.StatusRepository;
  * Interface used to define an OCPP message from a visitor.
  */
 public interface OcppObserver {
+
   /**
    * Instantiate an OcppObserver according to the used OCPP version.
    *
-   * @param ocppVersion used ocpp version
-   * @param ocppMessageSender websocket chanel to send message
+   * @param ocppVersion           used ocpp version
+   * @param ocppMessageSender     websocket chanel to send message
    * @param chargepointRepository charge point repository
-   * @param firmwareRepository firmware repository
-   * @param statusRepository status repository
+   * @param firmwareRepository    firmware repository
+   * @param statusRepository      status repository
    * @return OcppObserver
    */
   static OcppObserver instantiateFromVersion(OcppVersion ocppVersion,
@@ -27,9 +29,18 @@ public interface OcppObserver {
                                              FirmwareRepository firmwareRepository,
                                              StatusRepository statusRepository) {
     return switch (ocppVersion) {
-      case V1_6 -> new Ocpp16ConfigurationObserver(ocppMessageSender,
-                      chargepointRepository, firmwareRepository, statusRepository);
-      case V2 -> throw new AssertionError("Still not implemented");
+      case V1_6 -> new Ocpp16ConfigurationObserver(
+              ocppMessageSender,
+              chargepointRepository,
+              firmwareRepository,
+              statusRepository
+      );
+      case V2 -> new OcppConfigurationObserver2(
+              ocppMessageSender,
+              chargepointRepository,
+              firmwareRepository,
+              statusRepository
+      );
     };
   }
 
