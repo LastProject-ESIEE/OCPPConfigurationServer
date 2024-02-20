b6211db43fa1f9d37efdf57552f06afe93439105
maxime.communiau <59145921+Idhao@users.noreply.github.com>
2024-02-20 16:53:02 +0100

    add : more datas to h2 database

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,5 +1,16 @@
 INSERT INTO type_allowed  (id_type_allowed, constructor, type) VALUES (1, 'Alfen', 'Eve Single S-line');
-INSERT INTO firmware (id_firmware, url, version, constructor) VALUES (1, 'www.alfen.com', '5.4.3-4073)', 'Alfen');
+INSERT INTO type_allowed  (id_type_allowed, constructor, type) VALUES (2, 'Alfen', 'Eve Double S-line');
+
+INSERT INTO firmware (id_firmware, url, version, constructor) VALUES (1, 'www.alfen5-4-3.com', '5.4.3-4073)', 'Alfen');
+INSERT INTO firmware (id_firmware, url, version, constructor) VALUES (2, 'www.alfen-5-3-1.com', '5.3.1-8374)', 'Alfen');
+
 INSERT INTO compatibility (id_firmware, id_type_allowed) VALUES (1, 1);
-INSERT INTO status (id_status, error) VALUES (1, '');
-INSERT INTO chargepoint (id_chargepoint, serial_number_chargepoint, type, constructor, client_id, server_address, configuration, id_status, id_firmware) VALUES (1, 'alfen_serial_number', 'mega-type', 'alfen', 'COMMUNIAU_1', 'localhost', '{json}', 1, 1);
+INSERT INTO compatibility (id_firmware, id_type_allowed) VALUES (1, 2);
+INSERT INTO compatibility (id_firmware, id_type_allowed) VALUES (2, 1);
+INSERT INTO compatibility (id_firmware, id_type_allowed) VALUES (2, 2);
+
+INSERT INTO status (error) VALUES ('');
+INSERT INTO status (error) VALUES ('');
+
+INSERT INTO chargepoint (serial_number_chargepoint, type, constructor, client_id, server_address, configuration, id_status, id_firmware) VALUES ('alfen_serial_number', 'mega-type', 'alfen', 'COMMUNIAU_1', 'localhost', '{json}', 1, 1);
+INSERT INTO chargepoint (serial_number_chargepoint, type, constructor, client_id, server_address, configuration, id_status, id_firmware) VALUES ('alfen_serial_number2', 'mega-type', 'alfen', 'COSTANDINI_1', 'localhost', '{json}', 2, 2);
