cd3029f330b6d90a819ecae48391f67c15905978
Thomas VELU <velu.thomas77@laposte.net>
2024-02-20 17:07:27 +0100

    fix : javadoc

--- src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp2/SetVariablesResponse20.java
+++ src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp2/SetVariablesResponse20.java
@@ -7,7 +7,7 @@ import java.util.List;
 /**
  * Request sent by the server to change the charge point configuration.
  *
- * @param setVariableResult test.
+ * @param setVariableResult {@link SetVariableResult}.
  */
 public record SetVariablesResponse20(List<SetVariableResult> setVariableResult)
         implements OcppMessage {
