770681c330b358494c3c5c1710108004316131f5
Thomas VELU <velu.thomas77@laposte.net>
2024-02-22 11:51:51 +0100

    add : classes to handle the firmware update in OCPP 1.6 protocol

---
+++ src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp16/FirmwareStatusNotificationRequest16.java
@@ -0,0 +1,10 @@
+package fr.uge.chargepointconfiguration.chargepoint.ocpp.ocpp16;
+
+/**
+ * Packet sent by the machine to describe the current state of the firmware download.<br>
+ * The server should respond by a ...
+ *
+ * @param firmwareStatus An enum to describe the current status of the firmware download.
+ */
+public record FirmwareStatusNotificationRequest16(String firmwareStatus) {
+}
---
+++ src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp16/FirmwareStatusNotificationResponse16.java
@@ -0,0 +1,7 @@
+package fr.uge.chargepointconfiguration.chargepoint.ocpp.ocpp16;
+
+/**
+ * Empty packet sent by the server to acknowledge the {@link FirmwareStatusNotificationRequest16}.
+ */
+public record FirmwareStatusNotificationResponse16() {
+}
