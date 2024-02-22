7a035e62c573a229febe166347dc292d4c88c0ff
Thomas VELU <velu.thomas77@laposte.net>
2024-02-22 11:56:07 +0100

    fix : wrong javadoc

--- src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp16/FirmwareStatusNotificationRequest16.java
+++ src/main/java/fr/uge/chargepointconfiguration/chargepoint/ocpp/ocpp16/FirmwareStatusNotificationRequest16.java
@@ -2,7 +2,7 @@ package fr.uge.chargepointconfiguration.chargepoint.ocpp.ocpp16;
 
 /**
  * Packet sent by the machine to describe the current state of the firmware download.<br>
- * The server should respond by a ...
+ * The server should respond by a {@link FirmwareStatusNotificationResponse16}.
  *
  * @param firmwareStatus An enum to describe the current status of the firmware download.
  */
