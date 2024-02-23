bad8cba4b10a2e72db05d0f55a15bc0996b4994d
Adrien COSTANDI <adrien.costandi@gmail.com>
2024-02-23 10:20:12 +0100

    create keyValue

--- src/front/src/pages/CreateConfig.tsx
+++ src/front/src/pages/CreateConfig.tsx
@@ -1,18 +1,5 @@
-import React, { SyntheticEvent, useState } from 'react';
-import {
-    Autocomplete,
-    AutocompleteChangeDetails,
-    AutocompleteChangeReason,
-    Box,
-    Button,
-    Container,
-    Grid,
-    Input,
-    MenuItem,
-    Paper,
-    Select,
-    TextField
-} from '@mui/material';
+import React, { useState } from 'react';
+import { Autocomplete, Box, Button, Container, Grid, Input, MenuItem, Paper, Select, TextField } from '@mui/material';
 import confKeys from "../conf/confKeys";
 import Typography from "@mui/material/Typography";
 
@@ -60,54 +47,30 @@ const FirmwareSection = () => (
 
 
 function KeyValuePair(props: {
-    selectedKeys: Set<string>,
-    setSelectedKeys: React.Dispatch<React.SetStateAction<Set<string>>>,
-    options: string[],
-    setOptions: React.Dispatch<React.SetStateAction<string[]>>
+    selectedKey: string
 }): JSX.Element {
-    const {selectedKeys, setSelectedKeys, options, setOptions} = props;
+    const {selectedKey} = props;
 
     const [currentValue, setCurrentValue] = useState("");
 
-    function handleChange(event: SyntheticEvent<Element, Event>, newValue: string | null, reason: AutocompleteChangeReason, details?: AutocompleteChangeDetails<string> | undefined) {
-        const deleteSelection = () => {
-            selectedKeys.delete(currentValue);
-            setSelectedKeys(selectedKeys);
-            setOptions(confKeys
-                .filter(key => !selectedKeys.has(key.keyName))
-                .map(key => key.keyName))
-        }
-        if (newValue === null) {
-            return;
-        }
-
-        deleteSelection();
-        if (reason === "clear" || !options.includes(newValue)) {
-            return;
-        }
-        setCurrentValue(newValue);
-        setSelectedKeys(selectedKeys.add(newValue));
-        setOptions(confKeys
-            .filter(key => !selectedKeys.has(key.keyName))
-            .map(key => key.keyName));
-
-    }
-
     return (
         <Grid sx={{pt: 1, pb: 1}} container alignItems="center" justifyContent="space-evenly">
-            <Grid item>
-                <Autocomplete
-                    onChange={handleChange}
-                    sx={{width: 300}}
-                    disablePortal
-                    options={options}
-                    renderInput={(params) => <TextField {...params} label="Clé"/>}/>
-            </Grid>
-            <Grid item>
-                <p>: </p>
-            </Grid>
-            <Grid item>
-                <Input placeholder="valeur"/>
+            <Grid sx={{pt: 1, pb: 1}} container alignItems="center" justifyContent="space-evenly">
+                <Grid item>
+                    <Typography>{selectedKey}</Typography>
+                </Grid>
+                <Grid item>
+                    <p>:</p>
+                </Grid>
+                <Grid item>
+                    <Input
+                        onChange={event => {
+                            setCurrentValue(event.target.value)
+                            console.log(selectedKey, event.target.value)
+                        }}
+                        value={currentValue}
+                        placeholder="valeur"/>
+                </Grid>
             </Grid>
         </Grid>
     )
@@ -133,49 +96,52 @@ function KeyValueSection() {
     return (
         <Box>
             <h4>Clé: valeur :</h4>
-            <Box sx={{ pt: 1, pb: 1 }} style={{ maxHeight: '60vh', overflow: 'auto' }}>
-                <Paper elevation={2} sx={{ p: 2 }}>
+            <Box sx={{pt: 1, pb: 1}} style={{maxHeight: '60vh', overflow: 'auto'}}>
+                <Paper elevation={2} sx={{p: 2}}>
                     <Grid container alignItems="center" justifyContent="space-evenly">
                         <Grid item>
-                            <Input disabled defaultValue="Borne_id" placeholder="Borne_id" />
+                            <Input disabled defaultValue="Borne_id" placeholder="Borne_id"/>
                         </Grid>
                         <Grid item>
                             <p>:</p>
                         </Grid>
                         <Grid item>
-                            <Input defaultValue="BRS-HERR" placeholder="valeur" />
+                            <Input defaultValue="BRS-HERR" placeholder="valeur"/>
                         </Grid>
                     </Grid>
                 </Paper>
 
-                <Paper elevation={2} sx={{ p: 2 }}>
+                <Paper elevation={2} sx={{p: 2}}>
                     <Grid container alignItems="center" justifyContent="space-evenly">
                         <Grid item>
                             <Autocomplete
                                 onChange={(event, value) => {
                                     setSelectedKey(value);
                                 }}
-                                sx={{ width: 300 }}
+                                sx={{width: 300}}
                                 disablePortal
                                 options={options}
                                 value={selectedKey}
-                                renderInput={(params) => <TextField {...params} label="Clé" />}
+                                renderInput={(params) => <TextField {...params} label="Clé"/>}
                             />
                         </Grid>
                         <Grid item>
-                            <Button onClick={updateOptions} variant="contained" type="submit" sx={{ borderRadius: 28 }}>
+                            <Button onClick={updateOptions} variant="contained" type="submit" sx={{borderRadius: 28}}>
                                 +
                             </Button>
                         </Grid>
                     </Grid>
                 </Paper>
                 {selectedKeys.length !== 0 && (
-                    <Paper elevation={2} sx={{ p: 2, mt: 2 }}>
-                        {selectedKeys.map((key) => (
-                            <Typography key={key} sx={{ mt: 2 }} variant="body1">
-                                {key} :
-                            </Typography>
-                        ))}
+                    <Paper elevation={2} sx={{p: 2, mt: 2}}>
+                        <Grid sx={{pt: 1, pb: 1}} container alignItems="center" justifyContent="space-evenly">
+
+                            {selectedKeys.map((key) => {
+                                return (
+                                    <KeyValuePair key={key} selectedKey={key} />
+                                )
+                            })}
+                        </Grid>
                     </Paper>
                 )}
             </Box>
