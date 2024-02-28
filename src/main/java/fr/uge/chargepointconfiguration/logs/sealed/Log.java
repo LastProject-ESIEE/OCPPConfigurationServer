e7c4bccdaad7303465f5e2fe8f1518013c264ca5
maxime.communiau <59145921+Idhao@users.noreply.github.com>
2024-02-28 08:28:48 +0100

    missing javadoc

--- src/main/java/fr/uge/chargepointconfiguration/logs/sealed/Log.java
+++ src/main/java/fr/uge/chargepointconfiguration/logs/sealed/Log.java
@@ -1,11 +1,23 @@
 package fr.uge.chargepointconfiguration.logs.sealed;
 
+import org.apache.logging.log4j.Level;
+
 /**
  * Interface used to define a log.
  */
 public sealed interface Log permits BusinessLog, TechnicalLog {
 
+  /**
+   * Get the level as interface type.
+   *
+   * @return String version of {@link Level}
+   */
   String getLevel();
 
+  /**
+   * Display all the infos of a log.
+   *
+   * @return String containing of the datas from a log.
+   */
   String text();
 }
