cdfd3e18b0150c66d7c054fab97b469dbac8cd95
Thomas VELU <velu.thomas77@laposte.net>
2024-02-28 13:29:31 +0100

    fix : bad import and import order

--- src/main/java/fr/uge/chargepointconfiguration/firmware/FirmwareRepository.java
+++ src/main/java/fr/uge/chargepointconfiguration/firmware/FirmwareRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Repository for the firmware.
