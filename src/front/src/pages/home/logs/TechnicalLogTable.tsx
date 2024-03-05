cecee51f8378b9a65b4d929137e5e1cd44d285f3
maxime.communiau <59145921+Idhao@users.noreply.github.com>
2024-03-05 14:16:22 +0100

    fake accordion for business and technical logs

--- src/front/src/pages/home/logs/TechnicalLogTable.tsx
+++ src/front/src/pages/home/logs/TechnicalLogTable.tsx
@@ -1,5 +1,13 @@
-import { Box, Grid, ListItemButton, Tooltip, Typography } from "@mui/material";
-import React, { useEffect } from "react";
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Box,
+    Grid,
+    Tooltip,
+    Typography
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
 import {
     InfinityScrollItemsTable,
     InfinityScrollItemsTableProps,
@@ -7,6 +15,8 @@ import {
     TableColumnDefinition
 } from "../../../sharedComponents/DisplayTable";
 import { searchTechnicalLog, TechnicalLog } from "../../../conf/technicalLogController";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 // import { Link } from "react-router-dom";
 
 const PAGE_SIZE = 30; // Max items displayed in the technical log table
@@ -14,6 +24,7 @@ const PAGE_SIZE = 30; // Max items displayed in the technical log table
 const technicalLogTableColumns: TableColumnDefinition[] = [
     {
         title: "Date",
+        size:11.5/3,
         /*
         filter: {
           apiField: "containsTitle",
@@ -23,9 +34,15 @@ const technicalLogTableColumns: TableColumnDefinition[] = [
     },
     {
         title: "Composant",
+        size: 11.5/3,
     },
     {
         title: "Log",
+        size: 11.5/3,
+    },
+    {
+        title: "",
+        size: 0.5,
     }
 ]
 
@@ -37,7 +54,7 @@ function TechnicalLogTable() {
 
     useEffect(() => {
         searchTechnicalLog(PAGE_SIZE).then((result: PageRequest<TechnicalLog> | undefined) => {
-            if(!result){
+            if (!result) {
                 setError("Erreur lors de la récupération des logs techniques.")
                 return
             }
@@ -53,34 +70,23 @@ function TechnicalLogTable() {
         data: tableData,
         hasMore: hasMore,
         error: error,
-        onSelection: technicalLog => { console.log("Selected item : " + technicalLog.id) },
+        onSelection: technicalLog => {
+            console.log("Selected item : " + technicalLog.id)
+        },
         formatter: (technicalLog) => {
             return (
-                <Box key={"box-configuration-edit-path-" + technicalLog.id}  paddingTop={1} maxWidth={"true"}>
+                <Box key={"box-configuration-edit-path-" + technicalLog.id} paddingTop={1} maxWidth={"true"}>
                     {/*<Link key={"chargepoint-edit-path-" + technicalLog.id}  to={{ pathname: 'display/' + technicalLog.id}} style={{ textDecoration: 'none', paddingTop: 10 }}>*/}
-                        <ListItemButton style={{maxWidth: "true", height:"5vh", padding: 0, paddingTop: 3, borderRadius: 50, color: 'black', backgroundColor: '#E1E1E1'}}>
-                            <Grid container maxWidth={"true"} flexDirection={"row"} alignItems={"center"}>
-                                <Grid item xs={12/technicalLogTableColumns.length} maxWidth={"true"} justifyContent={"center"}>
-                                    <Typography variant="body1" align="center" noWrap={true}>{new Date(technicalLog.date).toLocaleString()}</Typography>
-                                </Grid>
-                                <Grid item xs={12/technicalLogTableColumns.length} maxWidth={"true"} justifyContent={"center"}>
-                                    <Typography variant="body1" align="center" noWrap={true}>{technicalLog.component}</Typography>
-                                </Grid>
-                                <Grid item xs={12/technicalLogTableColumns.length} maxWidth={"true"} justifyContent={"center"}>
-                                    <Tooltip title={technicalLog.completeLog}>
-                                        <Typography variant="body1" align="center" noWrap={true}>{technicalLog.completeLog}</Typography>
-                                    </Tooltip>
-                                </Grid>
-                            </Grid>
-                        </ListItemButton>
-                    {/*</Link>*/}
+                    <LogLineItemVMamar technicalLog={technicalLog}/>
+                    {/*</Link>*/
+                    }
                 </Box>
             )
         },
         fetchData: () => {
             const nextPage = currentPage + 1;
-            searchTechnicalLog(PAGE_SIZE,nextPage).then((result: PageRequest<TechnicalLog> | undefined) => {
-                if(!result){
+            searchTechnicalLog(PAGE_SIZE, nextPage).then((result: PageRequest<TechnicalLog> | undefined) => {
+                if (!result) {
                     setError("Erreur lors de la récupération des logs techniques.")
                     return
                 }
@@ -93,5 +99,71 @@ function TechnicalLogTable() {
 
     return InfinityScrollItemsTable(props)
 }
+
+function LogLineItemVMamar(props: { technicalLog: TechnicalLog }) {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <Box onClick={() => setOpen(!open)}
+             style={{
+                 paddingTop: 3,
+                 paddingBottom: 3,
+                 borderRadius: open ? 25 : 50,
+                 backgroundColor: '#E1E1E1'
+             }}>
+                <Grid container maxWidth={"true"}
+                      flexDirection={"row"}
+                      alignItems={"center"}>
+                    <Grid item xs={technicalLogTableColumns[0].size}
+                          justifyContent={"center"}>
+                        <Typography variant="body1"
+                                    align="center"
+                                    noWrap={true}>
+                            {new Date(props.technicalLog.date).toLocaleString()}</Typography>
+                    </Grid>
+                    <Grid item xs={technicalLogTableColumns[1].size}
+                          justifyContent={"center"}>
+                        <Typography variant="body1"
+                                    align="center"
+                                    noWrap={true}>
+                            {props.technicalLog.component}</Typography>
+                    </Grid>
+                    <Grid item xs={technicalLogTableColumns[2].size}
+                          justifyContent={"center"}>
+                        {!open
+                            ? <Tooltip title={props.technicalLog.completeLog}>
+                                <Typography variant="body1"
+                                            align="center"
+                                            noWrap={true}>
+                                    {props.technicalLog.completeLog}
+                                </Typography>
+                            </Tooltip>
+                            : <Typography variant="inherit"
+                                          align="center"
+                                          noWrap={true}
+                                          color={'rgb(130,130,130)'}>
+                                {props.technicalLog.completeLog}
+                            </Typography>}
+                    </Grid>
+                    <Grid item xs={technicalLogTableColumns[3].size}>
+                            <Typography align={"center"}>
+                                {!open
+                                    ? <ArrowDropDownIcon/>
+                                    : <ArrowDropUpIcon/>}
+                            </Typography>
+                    </Grid>
+                </Grid>
+            {open && (
+                <Typography align={"center"}
+                            marginLeft={"10vh"}
+                            marginRight={"10vh"}
+                            paddingTop={1}
+                            paddingBottom={1}>
+                    {props.technicalLog.completeLog}</Typography>
+            )}
+        </Box>
+    )
+}
+
 
 export default TechnicalLogTable;
