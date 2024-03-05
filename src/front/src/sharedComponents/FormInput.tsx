d8f0ba7ad55b6e7d72399009c46de9b82e701b30
Adrien COSTANDI <adrien.costandi@gmail.com>
2024-03-05 08:57:12 +0100

    add feature onError on FormInput

--- src/front/src/sharedComponents/FormInput.tsx
+++ src/front/src/sharedComponents/FormInput.tsx
@@ -10,6 +10,7 @@ export type FormInputProps = {
     checkIsWrong?: (value: string) => boolean,
     placeholder?: string,
     value?: string,
+    onError?: () => void
 }
 
 function FormInput({
@@ -19,13 +20,15 @@ function FormInput({
                        onChange,
                        checkIsWrong = val => false,
                        placeholder = name,
-                       value = ""
+                       value = "",
+                       onError = () => {},
                    }: FormInputProps) {
     const [actualBackground, setActualBackground] = useState(backgroundColor);
 
     useEffect(() => {
         if (checkIsWrong(value)) {
             setActualBackground('rgba(255, 0, 0, 0.2)')
+            onError()
         } else {
             setActualBackground(backgroundColor)
         }
