e21094c30fa901f2a09243e9fde1eb8e248732c8
Thomas VELU <velu.thomas77@laposte.net>
2024-03-06 18:54:21 +0100

    fix : format

--- src/front/src/pages/home/chargepoint/CreateChargepoint.tsx
+++ src/front/src/pages/home/chargepoint/CreateChargepoint.tsx
@@ -117,11 +117,15 @@ function CreateChargepoint(props: { id?: number }) {
                         {loading ? (
                             <>
                                 <Skeleton sx={{height: "7vh", p: 2, mt: 3, backgroundColor: 'rgb(249, 246, 251)'}}
-                                        variant="rounded"/>
-                                <Skeleton sx={{height: "7vh", p: 2, mt: 3, backgroundColor: 'rgb(249, 246, 251)'}} variant="rounded"/>
-                                <Skeleton sx={{height: "7vh", p: 2, mt: 3, backgroundColor: 'rgb(249, 246, 251)'}} variant="rounded"/>
-                                <Skeleton sx={{height: "7vh", p: 2, mt: 3, backgroundColor: 'rgb(249, 246, 251)'}} variant="rounded"/>
-                                <Skeleton sx={{height: "7vh", p: 2, mt: 3, backgroundColor: 'rgb(249, 246, 251)'}} variant="rounded"/>
+                                          variant="rounded"/>
+                                <Skeleton sx={{height: "7vh", p: 2, mt: 3, backgroundColor: 'rgb(249, 246, 251)'}}
+                                          variant="rounded"/>
+                                <Skeleton sx={{height: "7vh", p: 2, mt: 3, backgroundColor: 'rgb(249, 246, 251)'}}
+                                          variant="rounded"/>
+                                <Skeleton sx={{height: "7vh", p: 2, mt: 3, backgroundColor: 'rgb(249, 246, 251)'}}
+                                          variant="rounded"/>
+                                <Skeleton sx={{height: "7vh", p: 2, mt: 3, backgroundColor: 'rgb(249, 246, 251)'}}
+                                          variant="rounded"/>
                             </>
                         ) : (
                             <>
@@ -204,8 +208,8 @@ function CreateChargepoint(props: { id?: number }) {
                                     }}
                                     pt={2}
                                 >
-                                        <Button sx={{borderRadius: 28}} variant="contained" color="primary"
-                                                onClick={handleSubmit}>Valider</Button>
+                                    <Button sx={{borderRadius: 28}} variant="contained" color="primary"
+                                            onClick={handleSubmit}>Valider</Button>
                                 </Box>
                             </>
                         )}
