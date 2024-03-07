7806f6e5f7448ad4b9ff9b6a37a1b743efdb335a
AxelHumbert <axelhumbert5@gmail.com>
2024-03-07 11:39:40 +0100

    Edit AddAccount to add a Button to go to the list page associated

--- src/front/src/pages/home/account/AddAccount.tsx
+++ src/front/src/pages/home/account/AddAccount.tsx
@@ -1,8 +1,10 @@
-import { Box, Button, Grid } from "@mui/material";
+import { Box, Button, Grid, IconButton } from "@mui/material";
 import FormInput from "../../../sharedComponents/FormInput";
 import RoleComponent from "./components/RoleComponent";
 import { useEffect, useState } from "react";
 import { Role, createNewUser } from "../../../conf/userController";
+import { useNavigate } from "react-router-dom";
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
 
 function AddAccount() {
@@ -15,6 +17,7 @@ function AddAccount() {
     const [password2, setPassword2]= useState("");
     const [toast, setToast] = useState(false); // TODO Edit with the real toast later
     const [display, setDisplay] = useState(false); // TODO Edit with the real toast later
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (
@@ -28,80 +31,89 @@ function AddAccount() {
     }, [password1, password2]);
 
     return (
-        <Grid container sx={{alignContent: "center", width: "100%"}}>
-            <Grid item xs={12} sx={{ml: "35%", mr: "35%"}}>
-                <FormInput name={"Nom"}
-                    onChange={lastName => setLastName(lastName)}
-                    value={lastName}
-                />
-            </Grid>
-            <Grid item xs={12} sx={{ml: "35%", mr: "35%"}}>
-                <FormInput name={"Prénom"}
-                    onChange={firstName => setFirstName(firstName)}
-                    value={firstName}
-                />
-            </Grid>
-            <Grid item xs={12} sx={{ml: "35%", mr: "35%"}}>
-                <FormInput name={"Email"}
-                    onChange={mail => setMail(mail)}
-                    value={mail}
-                />
-            </Grid>
-            <Grid item xs={12} sx={{ml: "35%", mr: "35%"}}>
-                <FormInput name={"Mot de passe"}
-                    onChange={password => setPassword1(password)}
-                    isPassword={true}
-                    value={password1}
-                />
-            </Grid>
-            <Grid item xs={12} sx={{ml: "35%", mr: "35%"}}>
-                <FormInput name={"Confirmer le mot de passe"}
-                    onChange={password => setPassword2(password)}
-                    isPassword={true}
-                    value={password2}
-                />
-            </Grid>
-            <Grid item xs={12} sx={{ml: "35%", mr: "35%"}}>
-                <RoleComponent role={role} setRole={setRole}/>
-            </Grid>
-            <Grid item xs={12} sx={{ml: "35%", mr: "35%", mt: "1%", textAlign: "center"}}>
-            <Button 
-                sx={{borderRadius: 28}} 
-                variant="contained" 
-                color="primary"
-                disabled={isButtonDisabled}
-                onClick={() => {
-                        const user = {
-                            firstName: firstName,
-                            lastName: lastName, 
-                            email: mail, 
-                            password: password1, 
-                            role: role
+        <Grid>
+            <Grid container sx={{alignContent: "center", width: "100%"}}>
+                <Grid item xs={12}>
+                    <IconButton
+                        onClick={() => navigate("/home/account")}
+                    >
+                        <ArrowBackIosNewIcon/>
+                    </IconButton>
+                </Grid>
+                <Grid item xs={12} sx={{ml: "35%", mr: "35%"}}>
+                    <FormInput name={"Nom"}
+                        onChange={lastName => setLastName(lastName)}
+                        value={lastName}
+                    />
+                </Grid>
+                <Grid item xs={12} sx={{ml: "35%", mr: "35%"}}>
+                    <FormInput name={"Prénom"}
+                        onChange={firstName => setFirstName(firstName)}
+                        value={firstName}
+                    />
+                </Grid>
+                <Grid item xs={12} sx={{ml: "35%", mr: "35%"}}>
+                    <FormInput name={"Email"}
+                        onChange={mail => setMail(mail)}
+                        value={mail}
+                    />
+                </Grid>
+                <Grid item xs={12} sx={{ml: "35%", mr: "35%"}}>
+                    <FormInput name={"Mot de passe"}
+                        onChange={password => setPassword1(password)}
+                        isPassword={true}
+                        value={password1}
+                    />
+                </Grid>
+                <Grid item xs={12} sx={{ml: "35%", mr: "35%"}}>
+                    <FormInput name={"Confirmer le mot de passe"}
+                        onChange={password => setPassword2(password)}
+                        isPassword={true}
+                        value={password2}
+                    />
+                </Grid>
+                <Grid item xs={12} sx={{ml: "35%", mr: "35%"}}>
+                    <RoleComponent role={role} setRole={setRole}/>
+                </Grid>
+                <Grid item xs={12} sx={{ml: "35%", mr: "35%", mt: "1%", textAlign: "center"}}>
+                <Button 
+                    sx={{borderRadius: 28}} 
+                    variant="contained" 
+                    color="primary"
+                    disabled={isButtonDisabled}
+                    onClick={() => {
+                            const user = {
+                                firstName: firstName,
+                                lastName: lastName, 
+                                email: mail, 
+                                password: password1, 
+                                role: role
+                            }
+                            let returnValue = createNewUser(user)
+                            returnValue.then(value => {
+                                setToast(value)
+                                setDisplay(true)
+                            })
                         }
-                        let returnValue = createNewUser(user)
-                        returnValue.then(value => {
-                            setToast(value)
-                            setDisplay(true)
-                        })
                     }
-                }
-            >
-                Créer
-            </Button>
-            </Grid>
-            <Grid item xs={12} sx={{ml: "35%", mr: "35%", mt: "1%", textAlign: "center"}}>
-                <Box>
-                    {toast && display && (
-                        // TODO : Mettre le toast ici
-                        <div style={{color: "green"}}>L'utilisateur a été créé.</div>
-                        ) 
-                    }
-                    {!toast &&  display && (
-                        // TODO : Mettre le toast ici 
-                        <div style={{color: "red"}}>L'utilisateur existe déjà.</div>
-                        )                         
-                    }
-                </Box>
+                >
+                    Créer
+                </Button>
+                </Grid>
+                <Grid item xs={12} sx={{ml: "35%", mr: "35%", mt: "1%", textAlign: "center"}}>
+                    <Box>
+                        {toast && display && (
+                            // TODO : Mettre le toast ici
+                            <div style={{color: "green"}}>L'utilisateur a été créé.</div>
+                            ) 
+                        }
+                        {!toast &&  display && (
+                            // TODO : Mettre le toast ici 
+                            <div style={{color: "red"}}>L'utilisateur existe déjà.</div>
+                            )                         
+                        }
+                    </Box>
+                </Grid>
             </Grid>
         </Grid>
     )
