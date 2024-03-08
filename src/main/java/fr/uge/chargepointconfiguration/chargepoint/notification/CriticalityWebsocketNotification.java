2bc0565cb4fc87b96501f457ee8fe9d98f9dfd1f
Thomas VELU <velu.thomas77@laposte.net>
2024-03-08 10:31:53 +0100

    fix : removed id

--- src/main/java/fr/uge/chargepointconfiguration/chargepoint/notification/CriticalityWebsocketNotification.java
+++ src/main/java/fr/uge/chargepointconfiguration/chargepoint/notification/CriticalityWebsocketNotification.java
@@ -3,9 +3,8 @@ package fr.uge.chargepointconfiguration.chargepoint.notification;
 /**
  * A message sent for the toast in the front.
  *
- * @param id The websocket message id.
  * @param content {@link CriticalityWebsocketContent}.
  */
-public record CriticalityWebsocketNotification(int id, CriticalityWebsocketContent content)
+public record CriticalityWebsocketNotification(CriticalityWebsocketContent content)
         implements WebSocketNotification {
 }
