d30ef7b8a053d0b4c6fefe57b7628f0a79756e67
Adrien COSTANDI <adrien.costandi@gmail.com>
2024-03-13 11:25:04 +0100

    added logs in for handled errors

--- src/main/java/fr/uge/chargepointconfiguration/errors/GlobalExceptionHandler.java
+++ src/main/java/fr/uge/chargepointconfiguration/errors/GlobalExceptionHandler.java
@@ -5,6 +5,10 @@ import fr.uge.chargepointconfiguration.errors.exceptions.EntityAlreadyExistingEx
 import fr.uge.chargepointconfiguration.errors.exceptions.EntityNotFoundException;
 import fr.uge.chargepointconfiguration.errors.exceptions.ForbiddenOperationException;
 import fr.uge.chargepointconfiguration.errors.exceptions.IllegalOperationException;
+import fr.uge.chargepointconfiguration.logs.CustomLogger;
+import fr.uge.chargepointconfiguration.logs.sealed.TechnicalLog;
+import fr.uge.chargepointconfiguration.logs.sealed.TechnicalLogEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -18,41 +22,52 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
  */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+  private final CustomLogger logger;
+
+  @Autowired
+  public GlobalExceptionHandler(CustomLogger logger) {
+    this.logger = logger;
+  }
 
   @ExceptionHandler(EntityNotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
   public ResponseEntity<ErrorMessage> handleNotFoundException(RuntimeException ex) {
+    logger.error(new TechnicalLog(TechnicalLogEntity.Component.BACKEND, ex.getMessage()));
     return new ResponseEntity<>(new ErrorMessage(ex.getMessage()), HttpStatus.NOT_FOUND);
   }
 
   @ExceptionHandler(EntityAlreadyExistingException.class)
   @ResponseStatus(HttpStatus.CONFLICT)
   public ResponseEntity<ErrorMessage> handleEntityAlreadyExistingException(RuntimeException ex) {
+    logger.error(new TechnicalLog(TechnicalLogEntity.Component.BACKEND, ex.getMessage()));
     return new ResponseEntity<>(new ErrorMessage(ex.getMessage()), HttpStatus.CONFLICT);
   }
 
   @ExceptionHandler(BadRequestException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ResponseEntity<ErrorMessage> handleBadRequestException(RuntimeException ex) {
+    logger.warn(new TechnicalLog(TechnicalLogEntity.Component.BACKEND, ex.getMessage()));
     return new ResponseEntity<>(new ErrorMessage(ex.getMessage()), HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(MethodArgumentTypeMismatchException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ResponseEntity<ErrorMessage> handleBadArguments(RuntimeException ex) {
-    return new ResponseEntity<>(new ErrorMessage("Les arguments passés ne sont pas valides"),
-        HttpStatus.BAD_REQUEST);
+    logger.error(new TechnicalLog(TechnicalLogEntity.Component.BACKEND, ex.getMessage()));
+    return new ResponseEntity<>(new ErrorMessage("Arguments invalides"), HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(IllegalOperationException.class)
   @ResponseStatus(HttpStatus.UNAUTHORIZED)
   public ResponseEntity<ErrorMessage> handleIllegalOperationException(RuntimeException ex) {
+    logger.error(new TechnicalLog(TechnicalLogEntity.Component.BACKEND, ex.getMessage()));
     return new ResponseEntity<>(new ErrorMessage(ex.getMessage()), HttpStatus.UNAUTHORIZED);
   }
 
   @ExceptionHandler({ ForbiddenOperationException.class, AccessDeniedException.class })
   @ResponseStatus(HttpStatus.FORBIDDEN)
   public ResponseEntity<ErrorMessage> handleForbiddenOperationException(RuntimeException ex) {
+    logger.error(new TechnicalLog(TechnicalLogEntity.Component.BACKEND, ex.getMessage()));
     return new ResponseEntity<>(new ErrorMessage(ex.getMessage()), HttpStatus.FORBIDDEN);
   }
 }
